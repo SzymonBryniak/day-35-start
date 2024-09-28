e53e6400696b6340b4ead9211be5fd9bcdfbea10
Szymon Bryniak <szymonbryniakproject@gmail.com>
2024-09-28 20:35:56 +0100

    day 35 progress (lesson 271 completed)

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import requests
+import smtplib
+
+
+password = "aruj ltqt spax soaw"
+username = "szymonbryniakproject@gmail.com"
+to_addrs = "oneplusszymonbryniak@gmail.com"
 
 api_key = "458f91702912ebbea1aea9bbdd8f4ace"
 endpoint = "https://api.openweathermap.org/data/2.5/forecast?"
@@ -16,17 +22,32 @@ print(response.status_code)
 my_weather = response.json()['list']
 
 
+def send_email(message):
+    message = "Subject: TEST \n\n {}".format(str(message))
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=username, password=password)
+        connection.sendmail(username, to_addrs, msg=message)
+
+
 def check_precipitation(weather):
+    to_send = []
+
     for i in weather:
         for key, value in i.items():
             if key == "weather":
                 for ii in value:
                     if ii.get('id') < 700:
                         print('Bring umbrella', ii.get('id'))
+                        to_send.append(f'Bring umbrella {ii.get('id')}')
                     else:
                         print(ii.get('main'))
+                        to_send.append(ii.get('main'))
             if key == "dt_txt":
                 print(value)
+                to_send.append(value)
+    send_email(to_send)
 
 
 check_precipitation(my_weather)
+
